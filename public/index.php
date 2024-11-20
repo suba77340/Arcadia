8a891ca57a10f6d1897b470d88a525fd393afbf3
suba77340 <suba01@hotmail.com>
2024-11-20 06:16:54 +0100

    Ajout du dossier public

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+require_once '../vendor/autoload.php'; // Inclure l'autoloader de Composer
+use App\Autoloader;
+use App\Config\Main;
+
+// constante contenant dossier racine du projet ARCADIA
+define('ROOT', dirname(__DIR__));
+//on importe autoloader
+require_once ROOT.'/src/Autoloader.php';
+Autoloader::register();
+
+// Main est le routeur 
+    $app = new Main();
+
+// Démarre l'application (start la méthode)
+    $app->start();
+
