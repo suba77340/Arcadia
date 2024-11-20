fa13ecda7f7cfd002fb7d9cd1acb8abac5d8a726
suba77340 <suba01@hotmail.com>
2024-11-20 09:57:45 +0100

    Ajout de la fonction loadEnv et mise à jour de index.php pour utiliser les variables d'environnement

---
+++ src/Config/loadenv.php
@@ -0,0 +1,21 @@
+<?php
+
+function loadEnv($path)
+{
+    if (!file_exists($path)) {
+        throw new Exception('.env file not found');
+    }
+
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) {
+            continue;
+        }
+
+        list($name, $value) = explode('=', $line, 2);
+        $_ENV[trim($name)] = trim($value);
+        putenv(sprintf('%s=%s', trim($name), trim($value)));
+    }
+}
+
+loadEnv(__DIR__ . '/.env');
