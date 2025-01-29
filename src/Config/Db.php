86f52e5072c93cc201424c6736fe59bf0029ab51
suba <suba01@hotmail.com>
2025-01-29 18:02:00 +0100

    modif Db.php

--- src/Config/Db.php
+++ src/Config/Db.php
@@ -10,11 +10,6 @@ class Db extends PDO
 
     private function __construct()
     {
-    
-        if (!isset($_ENV['HOST'], $_ENV['DBNAME'], $_ENV['USERNAME'], $_ENV['PASSWORD'])) {
-            throw new \Exception("Les variables d'environnement pour la base de données sont manquantes.");
-        }
-
         $dsn = 'mysql:host=' . $_ENV['HOST'] . ';dbname=' . $_ENV['DBNAME'];
         parent::__construct($dsn, $_ENV['USERNAME'], $_ENV['PASSWORD']);
         $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
