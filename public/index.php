78bf66214a71883673d3cc963fb0b27a0d92cd66
suba <suba01@hotmail.com>
2025-01-29 17:54:54 +0100

    modif index.php

--- public/index.php
+++ public/index.php
@@ -1,13 +1,13 @@
 <?php
 
+require_once ROOT . '/vendor/autoload.php';
 use App\Autoloader;
 use App\Config\Main;
 use Dotenv\Dotenv;  // Ici, tu importes correctement la classe Dotenv
 
+
 // constante contenant dossier racine du projet ARCADIA
 define('ROOT', dirname(__DIR__));
-
-require_once ROOT . '/vendor/autoload.php';
 
 // On importe l'autoloader
 require_once ROOT . '/src/Autoloader.php';
